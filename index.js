64ad717bcfee9c916ac941268ab88a2272721a64
53Abdeali <aliabdealifakhri53@gmail.com>
2023-12-27 17:04:10 +0530

    Loader Updated

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// Simulate content loading (you can replace this with your actual loading logic)
 window.addEventListener('load', function() {
-    // Simulate a delay (e.g., AJAX request) before hiding the loader
     setTimeout(function() {
-      document.getElementById('loader-container').style.display = 'none';
-      document.getElementById('main-content').style.display = 'block';
-    }, 2000); // Adjust the delay time as needed
+      document.getElementById('pulse').style.display = 'flex';
+      document.getElementById('pulse').style.display = 'none';
+    }, 4000);
   });
   
